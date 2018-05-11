de5a95136c2484d33faaa6cdadededa857a1363e
Arthur Jordan <arthur_jordan05@web.de>
2018-05-11 10:21:25 +0200

    Update TFT9341.h

--- TFT9341.h
+++ TFT9341.h
@@ -278,7 +278,58 @@
 #define swap(type, i, j) {type t = i; i = j; j = t;}
 #define fontbyte(x) pgm_read_byte(&cfont.font[x])
 
-#define SCANLINES 320
+#define SCANLINES 240
+//****************************************added for ST7789*************************************
+#define ST_CMD_DELAY   0x80    // special signifier for command lists
+
+#define ST77XX_NOP     0x00
+#define ST77XX_SWRESET 0x01
+#define ST77XX_RDDID   0x04
+#define ST77XX_RDDST   0x09
+
+#define ST77XX_SLPIN   0x10
+#define ST77XX_SLPOUT  0x11
+#define ST77XX_PTLON   0x12
+#define ST77XX_NORON   0x13
+
+#define ST77XX_INVOFF  0x20
+#define ST77XX_INVON   0x21
+#define ST77XX_DISPOFF 0x28
+#define ST77XX_DISPON  0x29
+#define ST77XX_CASET   0x2A
+#define ST77XX_RASET   0x2B
+#define ST77XX_RAMWR   0x2C
+#define ST77XX_RAMRD   0x2E
+
+#define ST77XX_PTLAR   0x30
+#define ST77XX_COLMOD  0x3A
+#define ST77XX_MADCTL  0x36
+
+#define ST77XX_MADCTL_MY  0x80
+#define ST77XX_MADCTL_MX  0x40
+#define ST77XX_MADCTL_MV  0x20
+#define ST77XX_MADCTL_ML  0x10
+#define ST77XX_MADCTL_RGB 0x00
+
+#define ST77XX_RDID1   0xDA
+#define ST77XX_RDID2   0xDB
+#define ST77XX_RDID3   0xDC
+#define ST77XX_RDID4   0xDD
+
+#define ST7789_240x240_XSTART 0
+#define ST7789_240x240_YSTART 80
+
+
+// Color definitions
+#define	ST77XX_BLACK   0x0000
+#define	ST77XX_BLUE    0x001F
+#define	ST77XX_RED     0xF800
+#define	ST77XX_GREEN   0x07E0
+#define ST77XX_CYAN    0x07FF
+#define ST77XX_MAGENTA 0xF81F
+#define ST77XX_YELLOW  0xFFE0
+#define ST77XX_WHITE   0xFFFF
+//****************************************added for ST7789*************************************
 
 struct _current_font
 {
